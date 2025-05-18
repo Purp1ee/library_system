0e6c4abe893d0695713d7e05450ae65ce2fc4dae
Purp1ee <dmitry613@mail.ru>
2025-05-18 21:07:16 +0300

    Change #3

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -1,64 +1,82 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Book } from '../../entities/book.entity';
+import { Author } from '../../entities/author.entity';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
-import { AuthorsService } from '../authors/authors.service';
 
 @Injectable()
 export class BooksService {
   constructor(
     @InjectRepository(Book)
-    private booksRepository: Repository<Book>,
-    private authorsService: AuthorsService,
+    private bookRepository: Repository<Book>,
+
+    @InjectRepository(Author)
+    private authorRepository: Repository<Author>,
   ) {}
 
   async findAll(): Promise<Book[]> {
-    return this.booksRepository.find({ relations: ['author'] });
+    return this.bookRepository.find({ relations: ['author'] });
   }
 
   async findOne(id: number): Promise<Book> {
-    const book = await this.booksRepository.findOne({
+    const book = await this.bookRepository.findOne({
       where: { id },
       relations: ['author'],
     });
     if (!book) {
-      throw new NotFoundException(`Book with ID ${id} not found`);
+      throw new NotFoundException(`Книга с id=${id} не найдена`);
     }
     return book;
   }
 
-  async create(createBookDto: CreateBookDto): Promise<Book> {
-    const author = await this.authorsService.findOne(createBookDto.authorId);
-    const book = this.booksRepository.create({
-      title: createBookDto.title,
-      yearPublished: createBookDto.yearPublished,
-      numberCopies: createBookDto.numberCopies,
-      dateReleaseBooks: new Date(createBookDto.dateReleaseBooks),
+  async create(dto: CreateBookDto): Promise<Book> {
+    const author = await this.authorRepository.findOne({
+      where: { id: dto.authorId },
+    });
+    if (!author) {
+      throw new NotFoundException('Автор не найден');
+    }
+
+    const book = this.bookRepository.create({
+      title: dto.title,
+      yearPublished: dto.yearPublished,
+      numberCopies: dto.numberCopies,
+      dateReleaseBooks: new Date(dto.dateReleaseBooks),
       author,
     });
-    return this.booksRepository.save(book);
+
+    return this.bookRepository.save(book);
   }
 
-  async update(id: number, updateBookDto: UpdateBookDto): Promise<Book> {
+  async update(id: number, dto: UpdateBookDto): Promise<Book> {
     const book = await this.findOne(id);
-    
-    if (updateBookDto.authorId) {
-      const author = await this.authorsService.findOne(updateBookDto.authorId);
+
+    if (dto.authorId !== undefined) {
+      const author = await this.authorRepository.findOne({
+        where: { id: dto.authorId },
+      });
+      if (!author) {
+        throw new NotFoundException('Автор не найден');
+      }
       book.author = author;
     }
 
-    if (updateBookDto.title) book.title = updateBookDto.title;
-    if (updateBookDto.yearPublished) book.yearPublished = updateBookDto.yearPublished;
-    if (updateBookDto.numberCopies) book.numberCopies = updateBookDto.numberCopies;
-    if (updateBookDto.dateReleaseBooks) book.dateReleaseBooks = new Date(updateBookDto.dateReleaseBooks);
+    if (dto.title !== undefined) book.title = dto.title;
+    if (dto.yearPublished !== undefined) book.yearPublished = dto.yearPublished;
+    if (dto.numberCopies !== undefined) book.numberCopies = dto.numberCopies;
+    if (dto.dateReleaseBooks !== undefined)
+      book.dateReleaseBooks = new Date(dto.dateReleaseBooks);
 
-    return this.booksRepository.save(book);
+    return this.bookRepository.save(book);
   }
 
   async remove(id: number): Promise<void> {
-    const book = await this.findOne(id);
-    await this.booksRepository.remove(book);
+    try {
+      await this.bookRepository.delete(id);
+    } catch (error) {
+      throw new BadRequestException('Невозможно удалить книгу');
+    }
   }
 }
